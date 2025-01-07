13a9db0cfa7a93147a69959e2fe693d6e55ce2a6
FWeidhas <felixweidhas@gmx.de>
2025-01-07 13:34:44 +0100

    route with instructions works

--- lib/pages/map.dart
+++ lib/pages/map.dart
@@ -90,79 +90,6 @@ class _FishingMapState extends State<FishingMap> {
     });
   }
 
-  // void _showFishingSpotDetails(FishingSpot spot) {
-  //   showModalBottomSheet(
-  //     context: context,
-  //     isScrollControlled: true,
-  //     backgroundColor: Colors.transparent,
-  //     builder: (BuildContext context) {
-  //       return DraggableScrollableSheet(
-  //         initialChildSize: 0.5, // 50% of screen height
-  //         minChildSize: 0.3, // 30% of screen height
-  //         maxChildSize: 0.8, // 80% of screen height
-  //         snap: true, // Enable snapping
-  //         snapSizes: const [0.3, 0.5, 0.8], // Define snap points
-  //         builder: (BuildContext context, ScrollController scrollController) {
-  //           return Container(
-  //             decoration: const BoxDecoration(
-  //               color: Colors.white,
-  //               borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
-  //             ),
-  //             child: ListView(
-  //               controller: scrollController,
-  //               padding: const EdgeInsets.all(16),
-  //               children: [
-  //                 // Drag handle
-  //                 Center(
-  //                   child: Container(
-  //                     width: 40,
-  //                     height: 4,
-  //                     margin: const EdgeInsets.symmetric(vertical: 8),
-  //                     decoration: BoxDecoration(
-  //                       color: Colors.grey[300],
-  //                       borderRadius: BorderRadius.circular(2),
-  //                     ),
-  //                   ),
-  //                 ),
-  //                 // Image section
-  //                 Container(
-  //                   height: 150,
-  //                   margin: const EdgeInsets.only(bottom: 16),
-  //                   alignment: Alignment.center,
-  //                   child: spot.picture != null
-  //                       ? Image.network(
-  //                           spot.picture!,
-  //                           fit: BoxFit.cover,
-  //                         )
-  //                       : const Icon(
-  //                           Icons.image_not_supported,
-  //                           size: 50,
-  //                           color: Colors.grey,
-  //                         ),
-  //                 ),
-  //                 // Title
-  //                 Text(
-  //                   spot.title ?? 'No Title',
-  //                   style: Theme.of(context)
-  //                       .textTheme
-  //                       .headlineMedium
-  //                       ?.copyWith(fontWeight: FontWeight.bold),
-  //                 ),
-  //                 const SizedBox(height: 8),
-  //                 // Description
-  //                 Text(
-  //                   spot.description ?? 'No Description',
-  //                   style: Theme.of(context).textTheme.bodyLarge,
-  //                 ),
-  //               ],
-  //             ),
-  //           );
-  //         },
-  //       );
-  //     },
-  //   );
-  // }
-
   Future<void> _calculateRoute(FishingSpot spot) async {
     if (userLocation == null) {
       _showToast("User location not available.");
@@ -195,38 +122,6 @@ class _FishingMapState extends State<FishingMap> {
       debugPrint("Error fetching route: $e");
       _showToast("Error calculating route.");
     }
-  }
-
-  List<LatLng> decodePolyline(String polyline) {
-    List<LatLng> points = [];
-    int index = 0, len = polyline.length;
-    int lat = 0, lng = 0;
-
-    while (index < len) {
-      int shift = 0, result = 0;
-      int b;
-      do {
-        b = polyline.codeUnitAt(index++) - 63;
-        result |= (b & 0x1F) << shift;
-        shift += 5;
-      } while (b >= 0x20);
-      int dlat = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
-      lat += dlat;
-
-      shift = 0;
-      result = 0;
-      do {
-        b = polyline.codeUnitAt(index++) - 63;
-        result |= (b & 0x1F) << shift;
-        shift += 5;
-      } while (b >= 0x20);
-      int dlng = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
-      lng += dlng;
-
-      points.add(LatLng(lat / 1E5, lng / 1E5));
-    }
-
-    return points;
   }
 
   void _showFishingSpotDetails(FishingSpot spot) {
@@ -401,7 +296,10 @@ class _FishingMapState extends State<FishingMap> {
                     },
                   ),
                   ElevatedButton(
-                    onPressed: () => _calculateRoute(spot),
+                    onPressed: () {
+                      Navigator.pop(context);
+                      _calculateRouteAndShowInstructions(spot);
+                    },
                     child: const Text("Calculate Route"),
                   ),
                 ],
@@ -411,6 +309,155 @@ class _FishingMapState extends State<FishingMap> {
         );
       },
     );
+  }
+
+  Future<void> _calculateRouteAndShowInstructions(FishingSpot spot) async {
+    if (userLocation == null) {
+      _showToast("User location not available.");
+      return;
+    }
+
+    final start = '${userLocation!.longitude},${userLocation!.latitude}';
+    final end = '${spot.longitude},${spot.latitude}';
+    final osrmUrl =
+        'https://router.project-osrm.org/route/v1/driving/$start;$end?overview=full&steps=true';
+
+    try {
+      final response = await http.get(Uri.parse(osrmUrl));
+      if (response.statusCode == 200) {
+        final data = json.decode(response.body);
+        final route = data['routes'][0];
+        final geometry = route['geometry'];
+        final steps = route['legs'][0]['steps'] as List;
+
+        final PolylinePoints polylinePoints = PolylinePoints();
+        final List<PointLatLng> decodedPoints =
+            polylinePoints.decodePolyline(geometry);
+        setState(() {
+          routeCoordinates = decodedPoints
+              .map((point) => LatLng(point.latitude, point.longitude))
+              .toList();
+        });
+
+        // Extract instructions from the steps
+        final instructions = steps.map((step) {
+          final distance = step['distance'];
+          final roadName = step['name'] ?? "";
+          final instruction = step['maneuver']['modifier'] ?? "";
+
+          return {
+            'instruction': instruction,
+            'distance': distance,
+            'road_name': roadName,
+          };
+        }).toList();
+
+        // Show the new bottom sheet with instructions
+        showModalBottomSheet(
+          context: context,
+          isScrollControlled: true,
+          backgroundColor: Colors.transparent,
+          builder: (BuildContext context) {
+            return DraggableScrollableSheet(
+              initialChildSize: 0.6,
+              minChildSize: 0.4,
+              maxChildSize: 0.9,
+              snap: true,
+              builder:
+                  (BuildContext context, ScrollController scrollController) {
+                return Container(
+                  decoration: BoxDecoration(
+                    color: Theme.of(context).colorScheme.secondaryContainer,
+                    borderRadius:
+                        const BorderRadius.vertical(top: Radius.circular(20)),
+                  ),
+                  child: ListView(
+                    controller: scrollController,
+                    padding: const EdgeInsets.all(16),
+                    children: [
+                      // Drag handle
+                      Center(
+                        child: Obx(() {
+                          final isDarkTheme =
+                              Get.find<ThemeController>().themeMode ==
+                                  ThemeMode.dark;
+                          return Container(
+                            width: 40,
+                            height: 4,
+                            margin: const EdgeInsets.symmetric(vertical: 8),
+                            decoration: BoxDecoration(
+                              color: isDarkTheme ? Colors.white : Colors.black,
+                              borderRadius: BorderRadius.circular(2),
+                            ),
+                          );
+                        }),
+                      ),
+                      // Title
+                      Text(
+                        "Route Instructions",
+                        style: Theme.of(context)
+                            .textTheme
+                            .headlineSmall
+                            ?.copyWith(fontWeight: FontWeight.bold),
+                      ),
+                      const SizedBox(height: 16),
+                      // Display the route instructions with icons
+                      ...instructions.map((step) {
+                        // Determine the icon based on the instruction
+                        IconData icon;
+                        switch (step['instruction']) {
+                          case 'right':
+                            icon = Icons.turn_right_outlined;
+                            break;
+                          case 'left':
+                            icon = Icons.turn_left_outlined;
+                            break;
+                          case 'straight':
+                          default:
+                            icon = Icons.straight_outlined;
+                            break;
+                        }
+                        // Format distance for meters/kilometers
+                        String formattedDistance;
+                        if (step['distance'] > 1000) {
+                          formattedDistance =
+                              "${(step['distance'] / 1000).toStringAsFixed(1)} km";
+                        } else {
+                          formattedDistance =
+                              "${step['distance'].toStringAsFixed(0)} m";
+                        }
+                        return Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 8),
+                          child: ListTile(
+                            leading: Icon(icon),
+                            title: Text("Distance: $formattedDistance"),
+                            subtitle: Text(step['road_name']),
+                          ),
+                        );
+                      }),
+                      ElevatedButton(
+                        onPressed: () {
+                          Navigator.pop(context);
+                          setState(() {
+                            routeCoordinates = [];
+                          });
+                        },
+                        child: const Text("Cancel route"),
+                      ),
+                    ],
+                  ),
+                );
+              },
+            );
+          },
+        );
+      } else {
+        _showToast("Failed to fetch route instructions.");
+      }
+    } catch (e) {
+      debugPrint("Error fetching route: $e");
+      _showToast("Error calculating route.");
+    }
   }
 
   @override
